CyanogenMod/android_hardware_samsung

power: Add double tap to wake support
Change-Id: Icb0b6d442544b39576ec74e70e97751d6bc70590

power/Android.mk
@@ -24,6 +24,10 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := power.c
 LOCAL_MODULE_TAGS := optional
 
+ifneq ($(TARGET_DT2W_PATH),)
+    LOCAL_CFLAGS := -DDT2W_PATH=\"$(TARGET_DT2W_PATH)\"
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 endif

Evolution-X-Devices/hardware_samsung

power/Android.mk
@@ -24,6 +24,10 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := power.c
 LOCAL_MODULE_TAGS := optional
 
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS := -DDT2W_PATH=\"$(TARGET_DT2W_PATH)\"
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 endif