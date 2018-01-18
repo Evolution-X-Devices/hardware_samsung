LineageOS/android_hardware_samsung

AdvancedDisplay: cyanogenmod -> lineageos
Change-Id: Ie6d64f0995f266d2dbe3e16c8032820cc5164c1f

AdvancedDisplay/src/com/lineageos/settings/device/AdvancedDisplayFragment.java
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.lineageos.settings.device;
 
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
 
-import com.cyanogenmod.settings.device.R;
+import com.lineageos.settings.device.R;
 
-import org.cyanogenmod.internal.util.FileUtils;
+import org.lineageos.internal.util.FileUtils;
 
 public class AdvancedDisplayFragment extends PreferenceFragment {
     private mDNIeScenario mmDNIeScenario;

AdvancedDisplay/src/com/lineageos/settings/device/BootCompletedReceiver.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2012 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +15,17 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.lineageos.settings.device;
 
-public class Constants {
-    static final String KEY_MDNIE_SCENARIO = "mdnie_scenario";
-    static final String KEY_MDNIE_NEGATIVE = "mdnie_negative";
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mDNIeScenario.restore(context);
+        mDNIeNegative.restore(context);
+    }
 }

Evolution-X-Devices/hardware_samsung

AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayFragment.java
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package org.lineageos.settings.device;
 
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
 
-import com.cyanogenmod.settings.device.R;
+import org.lineageos.settings.device.R;
 
-import org.cyanogenmod.internal.util.FileUtils;
+import org.lineageos.internal.util.FileUtils;
 
 public class AdvancedDisplayFragment extends PreferenceFragment {
     private mDNIeScenario mmDNIeScenario;

AdvancedDisplay/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2012 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,18 +15,17 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package org.lineageos.settings.device;
 
-import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
-import com.android.settingslib.drawer.SettingsDrawerActivity;
+public class BootCompletedReceiver extends BroadcastReceiver {
 
-public class AdvancedDisplayActivity extends SettingsDrawerActivity {
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
-                new AdvancedDisplayFragment()).commit();
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mDNIeScenario.restore(context);
+        mDNIeNegative.restore(context);
     }
 }