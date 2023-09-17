LineageOS/android_hardware_samsung

samsung: fingerprint: support optical sensors
Change-Id: I06d9aab53f3cd900247ca23b83eaff79dccecf46

hidl/fingerprint/Android.mk
@@ -52,6 +52,10 @@ ifeq ($(TARGET_SEC_FP_REQUEST_FORCE_CALIBRATE),true)
     LOCAL_CFLAGS += -DREQUEST_FORCE_CALIBRATE
 endif
 
+ifeq ($(TARGET_SEC_FP_OPTICAL_SENSOR),true)
+    LOCAL_CFLAGS += -DIS_OPTICAL_SENSOR
+endif
+
 LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.3-service.samsung
 LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.3-service.samsung.rc
 LOCAL_VINTF_FRAGMENTS := android.hardware.biometrics.fingerprint@2.3-service.samsung.xml

hidl/fingerprint/BiometricsFingerprint.cpp
@@ -102,10 +102,16 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
 }
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+#ifdef IS_OPTICAL_SENSOR
+    request(SEM_REQUEST_TOUCH_EVENT, 2);
+#endif
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
+#ifdef IS_OPTICAL_SENSOR
+    request(SEM_REQUEST_TOUCH_EVENT, 1);
+#endif
     return Void();
 }
 

Evolution-X-Devices/hardware_samsung

hidl/fingerprint/Android.mk
@@ -52,6 +52,10 @@ ifeq ($(TARGET_SEC_FP_REQUEST_FORCE_CALIBRATE),true)
     LOCAL_CFLAGS += -DREQUEST_FORCE_CALIBRATE
 endif
 
+ifeq ($(TARGET_SEC_FP_REQUEST_TOUCH_EVENT),true)
+    LOCAL_CFLAGS += -DREQUEST_TOUCH_EVENT
+endif
+
 LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.3-service.samsung
 LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.3-service.samsung.rc
 LOCAL_VINTF_FRAGMENTS := android.hardware.biometrics.fingerprint@2.3-service.samsung.xml

hidl/fingerprint/BiometricsFingerprint.cpp
@@ -102,10 +102,16 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
 }
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+#ifdef REQUEST_TOUCH_EVENT
+    request(SEM_REQUEST_TOUCH_EVENT, 2);
+#endif
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
+#ifdef REQUEST_TOUCH_EVENT
+    request(SEM_REQUEST_TOUCH_EVENT, 1);
+#endif
     return Void();
 }
 